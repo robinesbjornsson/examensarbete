6423e408eaf112fc35a888a034640c1e2ea35028
Robin Esbjörnsson <robin.esbjornsson@iths.se>
2022-06-03 09:45:21 +0200

    removed banner image

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import BannerImage from '../assets/bannerImage2.jpeg'
+
 const Banner = () => {
     return (
         <div className="relative h-[200px] sm:h-[200px] lg:h-[300px] xl:h-[300px] 2xl:h-[300px]">
-           <Image
-           src={BannerImage}
-           layout="fill"
-           objectFit="cover"
-           />
+         
            <div className="absolute top-1/2 w-full text-center font-bold">
                <h1 className="text-5xl text-gray-50"> What's for dinner? </h1>
 
